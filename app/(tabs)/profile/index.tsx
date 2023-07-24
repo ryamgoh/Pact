0f1845a773188efd7b2906e99b807b33611db009
Ryam <ryanngohenrui@gmail.com>
2023-07-24 23:44:02 +0800

    Profile Card

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -20,7 +20,11 @@ const ProfilePage = () => {
       style={Styled.MainScrollableCanvas}
       contentContainerStyle={{ alignItems: "center" }}
     >
-      <ProfileCard />
+      <ProfileCard
+        profilePhoto={
+          "https://images.unsplash.com/photo-1689888154384-00e7f1e191f4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80"
+        }
+      />
       <Text style={{ fontFamily: FONT.bold }}>
         {AuthStore.getRawState().user?.email}
       </Text>

3c3fded38ca8efd482ebd3574d46b5f1301864cc
Ryam <ryanngohenrui@gmail.com>
2023-07-25 23:13:44 +0800

    Abstracted chats

--- app/(tabs)/chats/index.tsx
+++ app/(tabs)/chats/index.tsx
@@ -3,10 +3,13 @@ import React from "react";
 import { FONT } from "../../../constants";
 import ChatCard from "../../../components/Chats/ChatCard";
 import Styled from "../../../styles/container";
+import chatsJson from "./chats.json";
 
 // import { ScrollView } from "react-native";
 
 const ChatsPage = () => {
+  const allChats = chatsJson;
+
   return (
     <ScrollView
       style={Styled.MainScrollableCanvas}
@@ -22,60 +25,15 @@ const ChatsPage = () => {
       >
         Chat
       </Text>
-      <ChatCard
-        profilePhoto={
-          "https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
-        }
-        name={"Cleon"}
-        chatStatus={"New chat"}
-        lastSeen={"Just now"}
-        streaks={100}
-      />
-      <ChatCard
-        profilePhoto={
-          "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
-        }
-        name={"Ryann"}
-        chatStatus={"New chat"}
-        lastSeen={"Just now"}
-        streaks={100}
-      />
-      <ChatCard
-        profilePhoto={
-          "https://images.unsplash.com/photo-1608389769338-3d5ceb3c1bc4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
-        }
-        name={"Shishir"}
-        chatStatus={"New chat"}
-        lastSeen={"Just now"}
-        streaks={100}
-      />
-      <ChatCard
-        profilePhoto={
-          "https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8eW91bmclMjB3b21hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"
-        }
-        name={"Kate"}
-        chatStatus={"New chat"}
-        lastSeen={"Just now"}
-        streaks={100}
-      />
-      <ChatCard
-        profilePhoto={
-          "https://images.unsplash.com/photo-1563178406-4cdc2923acbc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8eW91bmclMjB3b21hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"
-        }
-        name={"Celeste"}
-        chatStatus={"New chat"}
-        lastSeen={"Just now"}
-        streaks={100}
-      />
-      <ChatCard
-        profilePhoto={
-          "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8eW91bmclMjB3b21hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"
-        }
-        name={"Christy"}
-        chatStatus={"New chat"}
-        lastSeen={"Just now"}
-        streaks={100}
-      />
+      {allChats.map((chat) => (
+        <ChatCard
+          profilePhoto={chat.profilePhoto}
+          name={chat.name}
+          chatStatus={chat.chatStatus}
+          lastSeen={chat.lastSeen}
+          streaks={chat.streaks}
+        />
+      ))}
     </ScrollView>
   );
 };

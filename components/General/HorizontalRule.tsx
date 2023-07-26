e064ebe3afa51c2c2a960bea599d17fa0de048b2
Your Name <you@example.com>
2023-07-26 23:12:56 +0800

    resolved merge conflicts

--- components/General/HorizontalRule.tsx
+++ components/General/HorizontalRule.tsx
@@ -2,18 +2,11 @@ import React from "react";
 import { FlexAlignType, View } from "react-native";
 
 interface HorizontalRuleInterface {
-<<<<<<< HEAD
-  width: string;
-  height: string | number;
-  position?: string;
-  marginTop: number;
-  backgroundColor?: string;
-=======
   width: number | string | undefined;
   height: number | string | undefined;
   position: "auto" | FlexAlignType | undefined;
   marginTop?: number | string | undefined;
->>>>>>> 7f186822421b3051f42e92af4606f1ac9b37723e
+  backgroundColor?: string;
 }
 
 const HorizontalRule = ({

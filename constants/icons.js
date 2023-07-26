f60870d325fb9d7c83bda50d190a23c963dc515e
Shishir <shishirbychapur@gmail.com>
2023-07-27 01:32:10 +0800

    Add Reset Password

--- constants/icons.js
+++ constants/icons.js
@@ -1,12 +1,15 @@
-import chatStatusIcon from "../assets/icons/chatStatusImage.png";
 import cameraIcon from "../assets/icons/camera.png";
+import chatStatusIcon from "../assets/icons/chatStatusImage.png";
 import editButton from "../assets/icons/edit-button.png";
-import searchIcon from "../assets/icons/search.png";
 import filterIcon from "../assets/icons/filter.png";
+import searchIcon from "../assets/icons/search.png";
+import tickIcon from "../assets/icons/tick.png";
+
 export default {
   chatStatusIcon,
   cameraIcon,
   editButton,
   searchIcon,
   filterIcon,
+  tickIcon,
 };
